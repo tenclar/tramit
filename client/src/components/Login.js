082c19d39c8e899e653ee2ce4d1f53f4fc323c96
Tenclar Valus <tenclarvalus@gmail.com>
2019-09-17 11:24:56 -0500

    configuração do login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,21 +1,79 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {login } from './usuario/UsuarioApi'
 import './css/Login.css'
 
 class Login extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+
+            email: '',
+            password: '',
+            error:''
+        };
+
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e){
+        this.setState({ [e.target.name]: e.target.value  })
+
+    }
+    onSubmit(e){
+        e.preventDefault()
+        this.setState({ error:'' });
+
+        const user = {
+            email:this.state.email,
+            password:this.state.password
+        }
+        
+      /*   login(user).then(
+            res => {                 
+                if(res){
+                    this.props.history.push('/profile')
+                }else this.setState({ error: res.data.error })
+            }
+            
+        ) */
+        
+       
+       login(user).then(
+           (res) => this.props.history.push('/app'),
+           (err) => this.setState({ error: err.response.data.error })
+       )
+    }
+
+
     render() {
-        return (
+
+       const {error } = this.state
+         return (
             <div className="formLogin">
 
-            <form className="form-signin">
+            <form onSubmit={this.onSubmit} className="form-signin">
             <div className="fa-w-20 fa-2x">
             <i className="fab fa-accusoft fa-2x "></i> Tramit .::. <span className="font-weight-bold" >DOC</span>
             </div>
                 <hr/>
                 <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                <div className="  alert-danger text-center rounded mb-3 " >erro{ error }</div>
                 <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                <input 
+                    type="email" 
+                    id="inputEmail" 
+                    name='email' 
+                    value={this.state.email}  onChange={this.onChange} 
+                    className="form-control" placeholder="Email address" required autoFocus />
                 <label htmlFor="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                <input 
+                    type="password" 
+                    id="inputPassword" 
+                    name="password" 
+                    value={this.state.password} onChange={this.onChange} 
+                    className="form-control" placeholder="Password" required />
                     <div className="checkbox mb-3">
                         <label>
                         <input type="checkbox" value="remember-me"/> Remember me
